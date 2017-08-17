7bf0e288903820681f700f823048a3c65ecbbe33
Jerome Tang <Jerome.Tang@quest.com>
2017-08-17 15:33:38 +0800

    P7 update

--- graduation/py/visulization.py
+++ graduation/py/visulization.py
@@ -17,7 +17,10 @@ def load_data():
     return data
 
 # View 'size' of store's
-def viewSalesDataOverTime(data, group, size = 10):
+# group: The supported group catagory, refer to https://chrisalbon.com/python/pandas_group_data_by_time.html
+# aggregation: the aggregation method: sum, mean
+# size: the Store counts to be viewed, pick up randomly
+def viewSalesDataOverTime(data, group, aggregation= 'sum', size = 10):
     stores = np.random.choice(len(data['Store'].unique()),size)
     view_frames = data.drop(['Customers','Open','Promo','SchoolHoliday','StateHoliday'], axis =1)
     cols = 3
@@ -28,7 +31,13 @@ def viewSalesDataOverTime(data, group, size = 10):
         count = count + 1
         temp_store = view_frames[view_frames.Store == i]
         temp_store.index = temp_store['Date']
-        temp_store = temp_store.resample(group).sum()
+        if aggregation == 'sum':
+            temp_store = temp_store.resample(group).sum()
+        elif aggregation == 'mean':
+            temp_store = temp_store.resample(group).mean()
+        else:
+            print "Not supported aggregation type: " + aggregation
+            return
         plot_row_index = math.ceil(count / cols) - 1
         plot_col_index = count - plot_row_index * cols -1
         temp_store.plot(kind='line',x=temp_store.index,y='Sales',
