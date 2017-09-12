a3aa3aa797b84bda254633602ec353377a80fe66
Jerry Tang <JeromeTang@gmail.com>
2017-09-12 23:03:05 +0800

    p7 review update

--- graduation_project/network.py
+++ graduation_project/network.py
@@ -5,7 +5,7 @@ from keras.layers.core import Dense, Activation, Merge, Reshape
 from keras.layers.embeddings import Embedding
 
 # prepare final features for network, here we only takes 7 out of 8 initial features
-# ‘open’ is not so helpful for 
+# ‘open’ is not so helpful for prediction, refer to graduation document
 def split_features(X):
     X_list = []
 
